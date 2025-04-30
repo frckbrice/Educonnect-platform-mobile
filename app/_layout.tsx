49e2924116c16d4260873c31b047ec5988a7420c
frckbrice <init.default>
2025-04-30 23:01:58 +0100

    prepare to get onboarding data

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,30 @@
 
 import { ThemeProvider } from "@/context/theme.context";
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { Text, View } from "react-native";
+import {
+  Poppins_600SemiBold,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_700Bold,
+  Poppins_500Medium,
+  useFonts,
+} from "@expo-google-fonts/poppins";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Poppins_600SemiBold,
+    Poppins_300Light,
+    Poppins_700Bold,
+    Poppins_400Regular,
+    Poppins_500Medium,
+  });
+
+
   return (
     <ThemeProvider>
       <Stack screenOptions={{ headerShown: false }}>
